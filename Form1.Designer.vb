fae962c7ee7539811155e9c3473b5de1856e8a0a
SB5TV <oncename@gmail.com>
2023-10-05 19:06:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Label2 = New Label()
+        ComboBox1 = New ComboBox()
+        Label1 = New Label()
+        Button2 = New Button()
+        OpenFileDialog1 = New OpenFileDialog()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(28, 32)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(555, 27)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "C:\Users\SB5\Source\Repos\yoump\bin\Debug\net7.0-windows\001.mp4"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(430, 165)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(153, 29)
+        Button1.TabIndex = 3
+        Button1.Text = "Конвертировать"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.Location = New Point(28, 126)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(396, 97)
+        Label2.TabIndex = 5
+        Label2.Text = "Converting Video GPU CUDA"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.DropDownStyle = ComboBoxStyle.DropDownList
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"MP4 (MPEG-4)", "MKV (Matroska)", "AVI (Audio Video Interleave)", "MOV (QuickTime)", "FLV (Flash Video)", "TS (Transport Stream)"})
+        ComboBox1.Location = New Point(145, 77)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(223, 28)
+        ComboBox1.TabIndex = 6
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(28, 80)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(112, 20)
+        Label1.TabIndex = 7
+        Label1.Text = "Формат видео:"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(489, 76)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 8
+        Button2.Text = "Обзор"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' OpenFileDialog1
+        ' 
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(611, 232)
+        Controls.Add(Button2)
+        Controls.Add(Label1)
+        Controls.Add(ComboBox1)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Form1"
+        Text = "Converting Video CUDA"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,153 @@
+﻿Imports System.Diagnostics
+Imports System.Security.Cryptography.Xml
+Imports System.Text
+Imports System.Text.Encodings
+Imports System.Threading.Tasks
+Imports Microsoft.VisualBasic.Devices
+Imports System.IO
+Imports System.Windows.Forms
+
+
+Public Class Form1
+    Private Delegate Sub UpdateLabelDelegate(text As String)
+    Private Sub UpdateLabel(text As String)
+        If Label2.InvokeRequired Then
+            Dim d As New UpdateLabelDelegate(AddressOf UpdateLabel)
+            Label2.Invoke(d, text)
+        Else
+            Label2.Text = text
+        End If
+    End Sub
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim sFormat As String = "MP4"
+
+        If File.Exists(TextBox1.Text) Then
+            If ComboBox1.Text = "MP4 (MPEG-4)" Then
+                sFormat = "MP4"
+            End If
+            If ComboBox1.Text = "MKV (Matroska)" Then
+                sFormat = "MKV"
+            End If
+            If ComboBox1.Text = "AVI (Audio Video Interleave)" Then
+                sFormat = "AVI"
+            End If
+            If ComboBox1.Text = "MOV (QuickTime)" Then
+                sFormat = "MOV"
+            End If
+            If ComboBox1.Text = "FLV (Flash Video)" Then
+                sFormat = "FLV"
+            End If
+            If ComboBox1.Text = "TS (Transport Stream)" Then
+                sFormat = "TS"
+            End If
+
+
+            Dim filePath As String = Path.Combine(Application.StartupPath, "input." & sFormat)
+            Dim inputVideoPath As String = TextBox1.Text
+            Dim outputVideoPath As String = Application.StartupPath & "input." & sFormat
+
+
+            If File.Exists(filePath) Then
+
+                File.Delete(filePath)
+
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Await Task.Run(Sub()
+                                   ConvertVideoUsingCUDA(inputVideoPath, outputVideoPath)
+                               End Sub)
+
+                Button1.Enabled = True
+                Button2.Enabled = True
+
+            Else
+
+                Button1.Enabled = False
+                Button2.Enabled = False
+                Await Task.Run(Sub()
+                                   ConvertVideoUsingCUDA(inputVideoPath, outputVideoPath)
+                               End Sub)
+
+                Button1.Enabled = True
+                Button2.Enabled = True
+
+            End If
+        Else
+            MsgBox("Выберите видео файл", 16, "Ошибка файла")
+        End If
+
+    End Sub
+
+    Private Sub ConvertVideoUsingCUDA(inputPath As String, outputPath As String)
+        Try
+            Dim ffmpegPath As String = Application.StartupPath & "\ffmpeg.exe"
+            Dim arguments As String = $"-i ""{inputPath}"" -c:v h264_nvenc -preset fast -cq 15 -b:v 10M -pix_fmt yuv420p ""{outputPath}"""
+
+            Dim processStartInfo As New ProcessStartInfo(ffmpegPath, arguments) With {
+            .UseShellExecute = False,
+            .RedirectStandardOutput = True,
+            .RedirectStandardError = True,
+            .CreateNoWindow = True
+        }
+
+            Dim process As New Process With {
+            .StartInfo = processStartInfo
+        }
+
+            Dim outputBuilder As New StringBuilder()
+            Dim errorsBuilder As New StringBuilder()
+
+            AddHandler process.OutputDataReceived, Sub(sender, e)
+                                                       If e.Data IsNot Nothing Then
+                                                           outputBuilder.AppendLine(e.Data)
+                                                           UpdateLabel(e.Data)
+                                                       End If
+                                                   End Sub
+
+            AddHandler process.ErrorDataReceived, Sub(sender, e)
+                                                      If e.Data IsNot Nothing Then
+                                                          errorsBuilder.AppendLine(e.Data)
+                                                          UpdateLabel($"{e.Data}")
+                                                      End If
+                                                  End Sub
+
+            process.Start()
+            process.BeginOutputReadLine()
+            process.BeginErrorReadLine()
+            process.WaitForExit()
+
+            UpdateLabel("Conversion completed.")
+        Catch ex As Exception
+            UpdateLabel($"An error occurred: {ex.Message}")
+        End Try
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Text = "MP4 (MPEG-4)"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.Filter = "Файлы видео (*.mp4;*.mkv;*.avi;*.mov;*.flv;*.ts)|*.mp4;*.mkv;*.avi;*.mov;*.flv;*.ts|Все файлы (*.*)|*.*"
+        OpenFileDialog1.ShowDialog()
+        Dim selectedFilePath As String = OpenFileDialog1.FileName
+        TextBox1.Text = OpenFileDialog1.FileName
+    End Sub
+
+End Class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
